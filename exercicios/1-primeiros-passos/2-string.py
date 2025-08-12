2cb170f3702f1fc8117dc97e99966c6c38fff915
SimoneCunha <sicaf.cunha@gmail.com>
2025-08-12 13:59:23 +0000

    resolução do exercício de strings

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,33 +1,29 @@
-resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
+resumo = "Paloma é uma mulher de 46 anos que está querendo mudar de carreira, por isso está estudando 'Python'."
 
 # Imprima na tela a variável "resumo"
 print(resumo)
-
 # Imprima na tela apenas a segunda letra da variável
-resumo(0)
-
-
-
-
-
+resumo[1]
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
+resumo[23:25]
 
 # Imprima na tela o trecho final da variável
-
+resumo[67:]
 
 # Converta todos as letras para minúsculo e imprima na tela
-
+resumo.lower
 
 # Converta todas as letras para maiúscula e imprima na tela
-
+resumo.upper
 
 # Formate a frase para que a primeira letra de cada palavra seja maiúscula e imprima na tela
-
+resumo.title
 
 # Formate a frase para que apenas a primeira letra da frase seja maiúscula e imprima na tela
-
+resumo.capitalize
 
 # Imprima na tela uma string utilizando uma variável, usando o recurso string format
+idade = 46
+print(f"Paloma é uma mulher de {idade} anos que está querendo mudar de carreira, por isso está estudando 'Python'.")
 
