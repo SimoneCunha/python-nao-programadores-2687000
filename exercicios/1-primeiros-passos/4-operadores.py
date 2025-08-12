e844290a3970f0a379a79ba5493ba69b1cae493e
SimoneCunha <sicaf.cunha@gmail.com>
2025-08-12 19:52:44 +0000

    resolução exercício lista

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,32 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+>>> idade_na_formatura = ano_formatura - ano_nascimento
+>>> print(idade_na_formatura)
+21
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
+>>> ano_formatura > ano_nascimento
+True
+>>> ano_formatura < ano_nascimento
+False
+>>> ano_formatura == ano_nascimento
+False
+>>> ano_formatura <= ano_nascimento
+False
+>>> ano_formatura >= ano_nascimento
+True
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+>>> 21 == 21 and 20 == 20
+True
+>>> 21 == 21 and 19 == 20
+False
+>>> 21 == 21 or 19 == 20
+True
+>>> 19 == 21 or 19 == 20
+False
+>>> not 3 == 1
+True
+>>> not 3 == 3
+False
--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,38 @@
 # Crie uma lista apenas com elementos numéricos
+numerica = [1, 2, 3, 4, 5]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+linguagens = ['python', 'r', 'julia', 'simone', 'helbert']
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+linguagens[0:5]
+['python', 'r', 'julia', 'simone', 'helbert']
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+>>> linguagens[0:-1:2]
+['python', 'julia']
 
 # Remova da lista o último item
-
+>>> print(linguagens)
+['python', 'r', 'julia', 'simone']
+>>> linguagens.pop()
+'simone'
+>>> print(linguagens)
+['python', 'r', 'julia']
+>>> 
 # Insira na lista um novo item
-
+>>> print(linguagens)
+['python', 'r', 'julia']
+>>> linguagens_adicionais = ['simone', 'davi']
+>>> linguagens.extend(linguagens_adicionais)
+>>> print(linguagens)
+['python', 'r', 'julia', 'simone', 'davi']
+>>> 
 # Remova da lista um item específico
+>>> linguagens = ['python', 'r', 'julia', 'simone', 'helbert']
+>>> print(linguagens)
+['python', 'r', 'julia', 'simone', 'helbert']
+>>> linguagens.remove('helbert')
+>>> print(linguagens)
+['python', 'r', 'julia', 'simone']
+>>> 
